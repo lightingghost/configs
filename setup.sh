7a24cb7645b271becfb69dc07ac510195b0e614e
Zhenpeng Zhou <zhenp3ngzhou@gmail.com>
2021-06-07 23:50:29 -0700

    centos

---
+++ setup.sh
@@ -0,0 +1,9 @@
+
+ln -s ~/configs/vimrc ~/.vimrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s ~/configs/zshrc ~/.zshrc
+curl -L git.io/antigen > ~/antigen.zsh
+
+
