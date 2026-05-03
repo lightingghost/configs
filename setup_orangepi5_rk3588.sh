2d24c7dd2d856a86491c1f4960ec53e4d4098cb8
lightingghost <zhenp3ngzhou@gmail.com>
2026-05-02 17:03:21 -0700

    Create setup script for Orange Pi 5 RK3588
    This script sets up the Orange Pi 5 RK3588 by updating apt sources, changing the default user, setting the hostname, installing fish shell, disabling auto login, and blacklisting Wi-Fi drivers.

---
+++ setup_orangepi5_rk3588.sh
@@ -0,0 +1,53 @@
+## update apt sources
+
+sudo tee /etc/apt/sources.list > /dev/null << 'EOF'
+deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+#deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+#deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+
+deb http://security.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
+#deb-src http://security.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+#deb-src http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+EOF
+
+echo "deb [arch=arm64] https://download.docker.com/linux/debian/ bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+
+sudo apt update
+
+# change user name , must login as root
+
+sudo pkill -u orangepi
+sudo pkill -9 -u orangepi
+sudo usermod -l odin orangepi
+sudo usermod -d /home/odin -m odin
+sudo groupmod -n odin orangepi
+sudo chfn -f "odin" odin
+
+sudo hostnamectl set-hostname atlas
+sudo sed -i 's/orangepi5ultra/atlas/g' /etc/host
+
+# fish
+sudo apt install fish
+chsh -s /usr/bin/fish odin
+
+
+# disable auto login
+
+sudo rm -rf /lib/systemd/system/getty@.service.d/
+sudo rm -rf /lib/systemd/system/serial-getty@.service.d/
+
+# disable wifi
+echo "blacklist bcmdhd" | sudo tee /etc/modprobe.d/blacklist-wifi.conf > /dev/null
+sudo update-initramfs -u
+
+#powersave governor
+sudo apt install cpufrequtils
+echo 'GOVERNOR="powersave"' | sudo tee /etc/default/cpufrequtils > /dev/null
